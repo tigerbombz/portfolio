489368b8b8c5a2cbff4826d35e6ef227a73aa6f3
tigerbombz <madd1778@yahoo.com>
2016-04-04 21:21:20 -0700

    revised portfolio

--- app/Data.js
+++ app/Data.js
@@ -4,14 +4,14 @@ var Data = {
     hello: 'Design is not just what it looks like and feels like. Design is how it works.',
     image: 'img/PhotoGrid_1446769581428.jpg',
     name: 'Matthew Nguyen',
-    subtitle: 'Developer',
-    text: 'Whatever good things we build end up building us.'
+    subtitle: 'Full Stack Developer & Entreprenuer',
+    text: 'Whatever good things we build ends up building us.'
   },
   contact: {
     items: []
   },
   images: {
-    empty: 'img/empty.png'
+    empty:[]
   },
   layout: {
     footer: {
@@ -32,66 +32,61 @@ Data.contact.items['facebook'] = {
   name:     'Facebook'
 }
 Data.contact.items['twitter'] = {
-  href:     'https://twitter.com/TommyDANGerouss',
+  href:     'https://twitter.com',
   imageUrl: 'img/twitter.png',
   name:     'Twitter'
 }
 Data.contact.items['linkedin'] = {
-  href:     'https://www.linkedin.com/profile/view?id=182483014',
+  href:     'https://www.linkedin.com/in/matthew-nguyen-75259a57',
   imageUrl: 'img/linkedin.png',
   name:     'LinkedIn'
 }
 Data.contact.items['github'] = {
-  href:     'https://github.com/tommydangerous',
+  href:     'https://github.com/tigerbombz',
   imageUrl: 'img/github.png',
   name:     'GitHub'
 }
 Data.contact.items['angellist'] = {
-  href:     'https://angel.co/tommydangerous',
+  href:     'https://angel.co/',
   imageUrl: 'img/angellist.png',
   name:     'AngelList'
 }
 Data.contact.items['email'] = {
-  href:     'mailto:quantumventuress@gmail.com',
+  href:     'mailto:matthewaknguyen@gmail.com',
   imageUrl: 'img/email.png',
   name:     'Email'
 }
 
-Data.portfolio.items['onmyblock'] = {
-  appStoreUrl: 'https://itunes.apple.com/us/app/onmyblock/id737199914?mt=8',
-  description: 'Find, rent, and list the best-off campus housing through ' +
-    'our online student marketplace.',
+Data.portfolio.items['homie'] = {
+  description: 'Find friends, create moments, & make everlasting friendships.',
   count:    0,
-  id:       'onmyblock',
-  imageUrl: 'img/onmyblock.png',
-  name:     'OnMyBlock',
+  id:       'homie',
+  // imageUrl: 'img/onmyblock.png',
+  name:     'Homie',
   style: {
     backgroundColor: 'rgb(41, 184, 229)'
   },
-  subtitle: 'Ruby on Rails / iOS',
-  websiteUrl: 'onmyblock.com',
+  subtitle: 'Python',
+  websiteUrl: null,
 };
-Data.portfolio.items['dunzo'] = {
-  appStoreUrl: 'https://itunes.apple.com/us/app/dunzo/id701772884?mt=8',
-  description: 'Open the app and immediately begin writing down notes, ' +
-    'thoughts, and reminders.',
+Data.portfolio.items['spot_me'] = {
+  description: 'Come share your experience and knowledge with these workout articles.',
   count:    0,
-  id:       'dunzo',
-  imageUrl: 'img/dunzo.png',
-  name:     'Dunzo',
+  id:       'spot_me',
+  // imageUrl: 'img/dunzo.png',
+  name:     'Spot Me',
   style: {
     backgroundColor: 'rgb(0, 0, 0)'
   },
-  subtitle: 'iOS',
+  subtitle: 'Ruby on Rails',
   websiteUrl: null,
 };
 Data.portfolio.items['spadetree'] = {
-  appStoreUrl: 'https://itunes.apple.com/us/app/spadetree/id686320991?mt=8',
   description: 'Find or become a tutor for kids K-12, teaching skills ' +
     'that are not typically taught in school.',
   count:    0,
   id:       'spadetree',
-  imageUrl: 'img/spadetree.png',
+  // imageUrl: 'img/spadetree.png',
   name:     'SpadeTree',
   style: {
     backgroundColor: 'rgb(0, 0, 0)'
@@ -99,46 +94,46 @@ Data.portfolio.items['spadetree'] = {
   subtitle: 'Python / Django / iOS',
   websiteUrl: 'spadetree.com',
 };
-Data.portfolio.items['bite'] = {
-  appStoreUrl: 'https://itunes.apple.com/us/app/bite-app/id661010278?mt=8',
-  description: 'Know where and when your friends are eating and ' +
-    'share with them your plans.',
-  count:    0,
-  id:       'bite',
-  imageUrl: 'img/bite.png',
-  name:     'Bite',
-  style: {
-    backgroundColor: 'rgb(207, 4, 4)'
-  },
-  subtitle: 'Ruby on Rails / iOS',
-  websiteUrl: 'abiteapp.com',
-};
-Data.portfolio.items['skimreads'] = {
-  appStoreUrl: null,
-  description: 'Save snippets of content from the web and ' +
-    'discover what other people are reading.',
-  count:    0,
-  id:       'skimreads',
-  imageUrl: 'img/skimreads.png',
-  name:     'Skimreads',
-  style: {
-    backgroundColor: 'rgb(40, 40, 40)'
-  },
-  subtitle: 'Python / Django',
-  websiteUrl: 'skimreads.com',
-};
-Data.portfolio.items['flashingdeals'] = {
-  appStoreUrl: null,
-  description: 'Curated daily deals using an algorithm that finds ' +
-    'the most popular deals around the web.',
-  count:    0,
-  id:       'flashingdeals',
-  imageUrl: 'img/flashingdeals.png',
-  name:     'FlashingDeals',
-  style: {
-    backgroundColor: 'rgb(214, 249, 255)'
-  },
-  subtitle: 'Ruby on Rails',
-  websiteUrl: 'flashingdeals.com',
-};
+// Data.portfolio.items['bite'] = {
+//   appStoreUrl: 'https://itunes.apple.com/us/app/bite-app/id661010278?mt=8',
+//   description: 'Know where and when your friends are eating and ' +
+//     'share with them your plans.',
+//   count:    0,
+//   id:       'bite',
+//   imageUrl: 'img/bite.png',
+//   name:     'Bite',
+//   style: {
+//     backgroundColor: 'rgb(207, 4, 4)'
+//   },
+//   subtitle: 'Ruby on Rails / iOS',
+//   websiteUrl: 'abiteapp.com',
+// };
+// Data.portfolio.items['skimreads'] = {
+//   appStoreUrl: null,
+//   description: 'Save snippets of content from the web and ' +
+//     'discover what other people are reading.',
+//   count:    0,
+//   id:       'skimreads',
+//   imageUrl: 'img/skimreads.png',
+//   name:     'Skimreads',
+//   style: {
+//     backgroundColor: 'rgb(40, 40, 40)'
+//   },
+//   subtitle: 'Python / Django',
+//   websiteUrl: 'skimreads.com',
+// };
+// Data.portfolio.items['flashingdeals'] = {
+//   appStoreUrl: null,
+//   description: 'Curated daily deals using an algorithm that finds ' +
+//     'the most popular deals around the web.',
+//   count:    0,
+//   id:       'flashingdeals',
+//   imageUrl: 'img/flashingdeals.png',
+//   name:     'FlashingDeals',
+//   style: {
+//     backgroundColor: 'rgb(214, 249, 255)'
+//   },
+//   subtitle: 'Ruby on Rails',
+//   websiteUrl: 'flashingdeals.com',
+// };
 
